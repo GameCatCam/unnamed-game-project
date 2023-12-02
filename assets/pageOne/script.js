25f64e5779e611f78ccf9e5feacb1b88d095d364
Cameron Rawlins <cameronrawlins11@gmail.com>
2023-12-02 18:41:42 -0500

    finished first page

--- assets/pageOne/script.js
+++ assets/pageOne/script.js
@@ -10,30 +10,88 @@ playerChar.src = 'https://www.frankslaboratory.co.uk/downloads/shadow_dog.png'
 const spriteWidth = 575;
 const spriteHeight = 523
 
-let frameX = 0;
-let frameY = 4;
-let gameFrame = 0
-const staggerFrames = 3
-animationFrames = 10;
+let playerState = 'idle'
+const dropdown = document.getElementById('animations')
+dropdown.addEventListener('change', function(e) {
+    playerState = e.target.value
+})
 
+let frameX = 0;
+let frameY = 0;
+let gameFrame = 0
+const staggerFrames = 4
+
+const spriteAnimations = []
+const animationStates = [
+    {
+        name: 'idle',
+        frames: 7
+    },
+    {
+        name: 'jump',
+        frames: 7
+    },
+    {
+        name: 'fall',
+        frames: 7
+    },
+    {
+        name: 'run',
+        frames: 9
+    },
+    {
+        name: 'dizzy',
+        frames: 11
+    },
+    {
+        name: 'sit',
+        frames: 5
+    },
+    {
+        name: 'roll',
+        frames: 7
+    },
+    {
+        name: 'bite',
+        frames: 7
+    },
+    {
+        name: 'ko',
+        frames: 12
+    },
+    {
+        name: 'hit',
+        frames: 4
+    },
+]
+animationStates.forEach((state, index) => {
+    let frames = {
+        loc: [],
+    }
+    for (let j = 0; j < state.frames; j++) {
+        let positionX = j * spriteWidth
+        let positionY = index * spriteHeight
+        frames.loc.push({ x: positionX, y: positionY })
+    }
+    spriteAnimations[state.name] = frames
+})
 
 function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    let position = Math.floor(gameFrame/staggerFrames) % spriteAnimations[playerState].loc.length
+    let frameX = spriteWidth * position
+    let frameY = spriteAnimations[playerState].loc[position].y
     ctx.drawImage(
         playerChar, // Initial image
-        frameX * spriteWidth, // starting x for crop
-        frameY * spriteHeight, // starting y for crop
+        frameX, // starting x for crop
+        frameY, // starting y for crop
         spriteWidth, // crop width
         spriteHeight, // crop height
         0, // stretch x
         0, // stretch y
-        CANVAS_WIDTH, // stretch width
-        CANVAS_HEIGHT // stretch height
+        spriteWidth, // stretch width
+        spriteHeight // stretch height
     );
-    if (gameFrame % staggerFrames === 0) {
-        if (frameX < animationFrames) frameX++;
-        else frameX = 0;
-    }
 
     gameFrame++;
     requestAnimationFrame(animate)
