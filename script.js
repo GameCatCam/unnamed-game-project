afdb0ce379f929fb01658dfc847a31acab8ff491
Cameron Rawlins <cameronrawlins11@gmail.com>
2023-12-02 17:23:10 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+/**  @type {HTMLCanvasElement} */
+const canvas = document.getElementById('canvas1')
+const ctx = canvas.getContext('2d')
+
+const CANVAS_WIDTH = ctx.width = 600;
+const CANVAS_HEIGHT = ctx.height = 600;
